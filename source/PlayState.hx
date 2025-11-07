a611ccc531cb14edda34be55f9d7c5442ac939ee
matthewfroncek861-svg <matthewfroncek861@gmail.com>
2025-11-07 08:31:35 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2101,13 +2101,19 @@ class PlayState extends MusicBeatState
 		if (health > 2)
 			health = 2;
 
+		// Heath Icons
 		if (healthBar.percent < 20)
+		{
 			iconP1.animation.curAnim.curFrame = 1;
-			iconP2.animation.curAnim.curFrame = 2;
-		if (healthBar.percent > 80)
-			iconP1.animation.curAnim.curFrame = 2;
+				iconP2.animation.curAnim.curFrame = 2;
+		}
+		else if (healthBar.percent > 80)
+		{
 			iconP2.animation.curAnim.curFrame = 1;
-		else {
+				iconP1.animation.curAnim.curFrame = 2;
+		}
+		else
+		{
 			iconP2.animation.curAnim.curFrame = 0;
 			iconP1.animation.curAnim.curFrame = 0;
 		}
